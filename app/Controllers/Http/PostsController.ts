b56f41b3d36f07a416e941137b4aaa69ccca4237
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2023-07-14 20:00:44 +0100

    feat: CRUD

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,25 +1,58 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class PostsController {
   public async index({}: HttpContextContract) {
-    return 'Get all posts'
+    return {
+      data: await Post.all(),
+    }
   }
 
-  public async store({ response }: HttpContextContract) {
+  public async store({ request, response }: HttpContextContract) {
     response.status(201)
-    
-    return 'Create a new post'
+
+    return Post.create(request.only(['title', 'content']))
   }
 
-  public async show({ params }: HttpContextContract) {
-    return 'Get a post: ' + params.id
+  public async show({ params, response }: HttpContextContract) {
+    const post = await Post.find(params.id)
+
+    if (post) {
+      return { ...post.toJSON() }
+    } else {
+      response.status(404)
+
+      return { error: 'Post not found' }
+    }
   }
 
-  public async update({ params }: HttpContextContract) {
-    return 'Update a post: ' + params.id
+  public async update({ params, request, response }: HttpContextContract) {
+    const post = await Post.find(params.id)
+
+    if (post) {
+      const { title, content } = request.only(['title', 'content'])
+      post.title = title
+      post.content = content
+
+      return post.save()
+    } else {
+      response.status(404)
+
+      return { error: 'Post not found' }
+    }
   }
 
-  public async destroy({ params }: HttpContextContract) {
-    return 'Delete a post: ' + params.id
+  public async destroy({ params, response }: HttpContextContract) {
+    const post = await Post.find(params.id)
+
+    if (post) {
+      await post.delete()
+
+      return { message: 'Post deleted' }
+    } else {
+      response.status(404)
+
+      return { error: 'Post not found' }
+    }
   }
 }
